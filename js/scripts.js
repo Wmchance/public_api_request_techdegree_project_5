3eac2fa1cd668625d510d62fb809a5de38074c42
Will <wmchance@gmail.com>
2022-09-10 11:39:15 +0700

    Move search Event Listener to external function

--- js/scripts.js
+++ js/scripts.js
@@ -1,69 +1,9 @@
-// Checking for data provided by the api for single user
-// fetch('https://randomuser.me/api/')
-//     .then(res => res.json())
-//     .then(data => {
-//         console.log(data)
-//         console.log(data.results)
-//         console.log(data.results[0]),
-//         console.log(data.results[0].name.first),
-//         console.log(data.results[0].name.last),
-//         console.log(data.results[0].email),
-//         console.log(data.results[0].location.city),
-//         console.log(data.results[0].location.state)
-//     })
-
-// To pull needed info for 12 random users from api
-// fetch('https://randomuser.me/api/?results=12')
-//     .then(res => res.json())
-//     .then(data => {
-//         // console.log(data)
-//         // console.log(data.results)
-
-//         data.results.forEach(element => {
-//             // console.log(element),
-//             console.log(element.name.first),
-//             console.log(element.name.last),
-//             console.log(element.email),
-//             console.log(element.location.city),
-//             console.log(element.location.state),
-//             console.log(element.picture.medium)
-//         });
-//     })
-
-// Base info-card function
-// function createInfoCard(element) {
-//     const galleryDiv = document.getElementById('gallery');
-//     const cardDiv = document.createElement('div'); //card Div
-//     cardDiv.classList.add('card');
-//     galleryDiv.appendChild(cardDiv);
-//     const imgDiv = document.createElement('div'); //Employee img div
-//     imgDiv.classList.add('card-img-container');
-//     const imgLink = document.createElement('img');
-//     imgLink.classList.add('card-img');
-//     imgLink.src = 'london-edited.jpg';
-//     imgLink.alt = 'profile picture';
-//     imgDiv.appendChild(imgLink);
-//     cardDiv.appendChild(imgDiv);
-//     const infoDiv = document.createElement('div'); // Employee info div
-//     infoDiv.classList.add('card-info-container');
-//     const idHeader = document.createElement('h3');
-//     idHeader.id = 'name';
-//     idHeader.classList.add('card-name', 'cap');
-//     idHeader.innerText = 'first name last name';
-//     const emailP = document.createElement('p');
-//     emailP.classList.add('card-text');
-//     emailP.innerText = 'email';
-//     const locationP = document.createElement('p');
-//     locationP.classList.add('card-text', 'cap');
-//     locationP.innerText = 'city, state';
-//     infoDiv.appendChild(idHeader);
-//     infoDiv.appendChild(emailP);
-//     infoDiv.appendChild(locationP);
-//     cardDiv.appendChild(infoDiv);
-// }
 
 const url = 'https://randomuser.me/api/?results=12&nat=us';
 
+/*
+* Fetches information obj for 12 random users
+*/
 function fetchUsers(url) {
     fetch(url)
         .then(res => res.json())
@@ -104,24 +44,8 @@ function createSearchBar() {
     formElement.appendChild(input1);
     formElement.appendChild(input2);
     searchContainer.appendChild(formElement);
-    formElement.addEventListener('submit', (e) => {
-        // console.log(input1.value);
-        const cardNameCap = document.getElementsByClassName('card-name cap');
-        const modalCards = document.getElementsByClassName('modal-container');
-        for(let i=0; i<cardNameCap.length; i++) {
-            cardNameCap[i].parentElement.parentElement.style.display = 'none';
-            if(cardNameCap[i].textContent.toLowerCase().includes(input1.value.toLowerCase())) {
-                e.preventDefault();
-                cardNameCap[i].parentElement.parentElement.style.display = '';
-            } else {
-                modalCards[i].classList.remove('selected');
-            }
-            if(input1.value === '') {
-                cardNameCap[i].parentElement.parentElement.style.display = '';
-                modalCards[i].classList.add('selected');
-            }
-        }
-    })
+    
+    searchEventListener(formElement, input1);
 }
 
 /*
@@ -320,15 +244,24 @@ function createErrorMsg(info) {
 }
 
 /*
-* Fetches information obj for 12 random users and calls createInfoCard(obj) on each to display 
-* the created user info cards in the gallery div
+* Event Listeners
 */
-// fetch('https://randomuser.me/api/?results=12')
-//     .then(res => res.json())
-//     .then(data => {
-//         console.log(data.results),
-//         data.results.forEach(element => {
-//             createInfoCard(element);
-//         });
-//     })
-
+function searchEventListener(formElement, input1) {
+    formElement.addEventListener('submit', (e) => {
+        const cardNameCap = document.getElementsByClassName('card-name cap');
+        const modalCards = document.getElementsByClassName('modal-container');
+        for(let i=0; i<cardNameCap.length; i++) {
+            cardNameCap[i].parentElement.parentElement.style.display = 'none';
+            if(cardNameCap[i].textContent.toLowerCase().includes(input1.value.toLowerCase())) {
+                e.preventDefault();
+                cardNameCap[i].parentElement.parentElement.style.display = '';
+            } else {
+                modalCards[i].classList.remove('selected');
+            }
+            if(input1.value === '') {
+                cardNameCap[i].parentElement.parentElement.style.display = '';
+                modalCards[i].classList.add('selected');
+            }
+        }
+    })
+}
